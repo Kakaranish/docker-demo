45d23b6c0879faf14c0e41cf20e790a5845cb196
Kakaranish <stasiekgruz@gmail.com>
2020-12-06 12:54:24 +0000

    +added /log endpoint

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const moment = require('moment');
+const fs = require('fs');
 
 const app = express();
+
+app.get('/log', (req, res) => {
+    const logsPath = "./logs/logs.txt";
+    const data = `${moment.utc().format("YYYY-MM-DD hh:mm:ss")} | /log called\n`
+
+    fs.appendFile(logsPath, data, err => {
+        if (err) console.log(err);
+    });
+
+    console.log("Processed /log request")
+    res.send("Log appended\n");
+});
 
 app.use("*", (_, res) => {
     res.send(`${moment.utc().format("YYYY-MM-DD hh:mm:ss")} | Hello world\n`);
